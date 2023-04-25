303122c0785638be3de28f0bdc074560501a19ad
HunterX-7 <fraquin7@gmail.com>
2023-04-25 15:47:06 -0500

    Add root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   #  root "users#index"
+  root "recipes#my_recipes"
   get 'my-recipes', to: 'recipes#my_recipes'
   resources :recipes
 end
