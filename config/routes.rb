d038d21b84c26c7249e36e122341c0c322d05d6c
dadadon <davianberoni@yahoo.com>
2023-04-26 17:03:12 -0500

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   get 'public-recipes', to: 'recipes#public_recipes'
   get 'my-recipes', to: 'recipes#my_recipes'
   get 'my-foods', to: 'foods#my_foods'
-  resources :recipes
+  resources :recipes do
+    resources :recipe_foods
+  end  
   resources :foods
 end
