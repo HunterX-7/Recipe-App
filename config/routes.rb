669ea6a2132555675ba4a2192a47ede3eb4ecb26
dadadon <davianberoni@yahoo.com>
2023-04-27 10:42:43 -0500

    Update file

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   get 'public-recipes', to: 'recipes#public_recipes'
   get 'my-recipes', to: 'recipes#my_recipes'
   get 'my-foods', to: 'foods#my_foods'
+  put '/recipes/:id/toggle_public', to: 'recipes#toggle_public', as: 'toggle_public_recipe'
+
   resources :recipes do
     resources :recipe_foods
   end  
